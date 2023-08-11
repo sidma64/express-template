62d8a7898eaa0fdd73b373a34d393d8f62b72ee8
sidma64 <toprak.code@gmail.com>
2023-08-12 01:18:28 +0300

    Add .editorconfig

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
-import express, { Request, Response } from 'express';
-import logger from 'morgan';
+import express, { Request, Response } from "express";
+import logger from "morgan";
 
 const app = express();
 const port = 3000;
 
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
 });
 
-app.get('/about', (req: Request, res: Response) => {
-    res.send('Phone Number: +90 530 309 28 78 E-Mail: toprak.code@gmail.com');
+app.get("/about", (req: Request, res: Response) => {
+    res.send("Phone Number: +90 530 309 28 78 E-Mail: toprak.code@gmail.com");
 });
 
 app.listen(port, () => {
