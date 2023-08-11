c2dee13d5cd3cdbf188142aa794e110c47cbf977
Sidma64 <56298498+Sidma64@users.noreply.github.com>
2023-08-11 19:30:38 +0300

    Hello World! with express

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-import express from 'express';
+const express = require('express');
+const app = express();
+const port = 3000;
 
-console.log(express);
+app.get('/', (req: any, res: any) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
