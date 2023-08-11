2d0b419ddf415d1a4c9deffabe2b211efcd81a8c
sidma64 <toprak.code@gmail.com>
2023-08-12 00:10:07 +0300

    Add underscore to unused parameters

--- main.ts
+++ main.ts
@@ -8,11 +8,11 @@ app.use('/public', express.static('public'));
 
 app.use(logger('dev'));
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
-app.get('/about', (req: Request, res: Response) => {
+app.get('/about', (_req: Request, res: Response) => {
     res.send('Phone Number: +90 530 309 28 78 E-Mail: toprak.code@gmail.com');
 });
 
