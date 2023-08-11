53d98b1d3611ab8b2dfa180e19619d02b6ef94d9
Sidma64 <56298498+Sidma64@users.noreply.github.com>
2023-08-11 20:02:17 +0300

    Added morgan logger

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from 'express';
+import logger from 'morgan';
 const app = express();
 const port = 3000;
 
+app.use(logger('dev'));
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
+});
+
+app.get('/about', (req: Request, res: Response) => {
+    res.send('Phone Number: +90 530 309 28 78 E-Mail: toprak.code@gmail.com');
 });
 
 app.listen(port, () => {
